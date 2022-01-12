92566af7148d8d8a08d646a72f7d9b606fcdf6d1
nozim-coder <52530419+nozim-coder@users.noreply.github.com>
2022-01-12 13:17:44 +0500

    my app first commit

---
+++ tarjimon/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Lugat
+# Create your views here.
+def index(request):
+    soz = request.GET.get('q','')
+    if soz and soz!='':
+        natija = Lugat.objects.filter(inglizcha__contains=soz).all()[:5]
+    else:
+        natija = None    
+        # for data in natija:
+        #     print(data.uzbekcha)
+
+
+
+    return render(request,'index.html',{'q':soz,'natija':natija})
+# def salom2(request):
+#     return HttpResponse('mening sahifam !!!')
